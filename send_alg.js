7606410b624d95ee1e3b70736579cf3ef617decb
Zyxwee <jelm@st-andrews.ac.uk>
2018-11-03 12:25:52 +0000

    Figuring Connections #TheKingdomHadManyParties

--- send_alg.js
+++ send_alg.js
@@ -1,3 +1,13 @@
-function send_message() {
+function send_message(message) {
+    
+    var element = document.getElementById("right_board");
+    var node = document.createTextNode(message);
+
+    if (element == null) {
+        element = document.createElement("div");
+    }
+
+    element.appendChild(node);
+
     document.getElementById("message").innerHTML = "Hello there. *sass*";
 }
