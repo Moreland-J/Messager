4b91322a5b6467bfdd09c662cc4938fa7fb6c1b4
Zyxwee <jelm@st-andrews.ac.uk>
2018-11-03 17:15:33 +0000

    Improving JS #TheFunKindWithBubbles

--- send_alg.js
+++ send_alg.js
@@ -1,13 +1,20 @@
-function send_message(message) {
-    
+function read_textArea() {
+    var message = document.getElementById("enter").value;
+    console.log(message);
+    return message;
+}
+
+function send_message() {
+    console.log("entered");
     var element = document.getElementById("right_board");
-    var node = document.createTextNode(message);
+    var node = document.createTextNode(read_textArea());
 
     if (element == null) {
         element = document.createElement("div");
+        element.setAttribute("id", "right_board");
     }
 
+    console.log(node);
     element.appendChild(node);
-
-    document.getElementById("message").innerHTML = "Hello there. *sass*";
+    console.log("appended");
 }
