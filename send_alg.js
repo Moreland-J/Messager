004914eefcbc0c30592c418ddf2433862053d9bb
Zyxwee <jelm@st-andrews.ac.uk>
2018-11-04 01:08:52 +0000

    Set up divs #ButThereWasOneTuneTheyParticularlyLoved

--- send_alg.js
+++ send_alg.js
@@ -1,20 +1,18 @@
 function read_textArea() {
     var message = document.getElementById("enter").value;
-    console.log(message);
     return message;
 }
 
 function send_message() {
-    console.log("entered");
     var element = document.getElementById("right_board");
     var node = document.createTextNode(read_textArea());
 
-    if (element == null) {
-        element = document.createElement("div");
-        element.setAttribute("id", "right_board");
-    }
-
-    console.log(node);
     element.appendChild(node);
-    console.log("appended");
+}
+
+function get_message(recevied) {
+    var element = document.getElementById("left_board");
+    var node = received;
+
+    element.appendChild(node);
 }
